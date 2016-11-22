f5f2e53367cf3dd5834c827a366c2f7539dd24d4
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2016-11-22 20:51:24 +1100

    Can read instructions from file into buffer

--- getfucked.cpp
+++ getfucked.cpp
@@ -1,6 +1,23 @@
 #include "getfucked.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main() {
-	std::cout << "Test C++ file" << std::endl;
+// Slurp files contents into a string
+std::string slurp(std::ifstream& in) {
+    std::stringstream sstr;
+    sstr << in.rdbuf();
+    return sstr.str();
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage ./getfucked <brainfuckfile>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Slurp source code into a string
+    std::string instructions{};
+    std::ifstream source(argv[1]);
+    if (source.is_open()) { instructions = slurp(source); }
 }
