f444486fe4de73e427f6d788c41f16e3593fce20
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2016-11-22 21:13:32 +1100

    Working without loops

--- getfucked.cpp
+++ getfucked.cpp
@@ -2,12 +2,56 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <array>
 
 // Slurp files contents into a string
 std::string slurp(std::ifstream& in) {
     std::stringstream sstr;
     sstr << in.rdbuf();
     return sstr.str();
+}
+
+void bracketOpen() {
+    return;
+}
+
+void bracketClosed() {
+    return;
+}
+
+void interpret(std::string& instructions) {
+    int head{0};
+    std::array<char, TAPE_SIZE> tape{};
+
+    for (auto instr = instructions.cbegin(); instr != instructions.cend(); ++instr) {
+        //std::cout << *instr << std::endl;
+        switch (*instr) {
+            case '>' : 
+                ++head; 
+                break;
+            case '<' :
+                --head; 
+                break;
+            case '+' : 
+                ++tape[head]; 
+                break;
+            case '-' : 
+                --tape[head];
+                break;
+            case '.' : 
+                std::cout << tape[head];
+                break;
+            case ',' : 
+                std::cin >> tape[head];
+                break;
+            case '[' :
+                bracketOpen();
+                break;
+            case ']' :
+                bracketClosed();
+                break;
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -17,7 +61,9 @@ int main(int argc, char* argv[]) {
     }
 
     // Slurp source code into a string
-    std::string instructions{};
+    std::string instr{};
     std::ifstream source(argv[1]);
-    if (source.is_open()) { instructions = slurp(source); }
+    if (source.is_open()) { instr = slurp(source); }
+
+    interpret(instr);
 }
